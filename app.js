b69e1f44b327e34cf8aff955ce84099ad79840d5
KRapes <kerri.rapes@gmail.com>
2015-08-14 05:03:21 -0500

    removed beams to Ryans left side on stuarttt picture. Renamed it Stuarttt2. Added javascript to fadeout banner and pitch while fading in calander

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 var main = function(){
 	
+	$('.btn').click(function() {
+		$('.pitch').fadeOut(600);
+		//$('.txt1').fadeOut(600);
+		//$('.txt2').fadeOut(600);
+		$('.banner').fadeOut(600);
+		//$('.calander').fadeIn(600).addClass('flexbox');
+		//$('.calander').fadeIn(600);
+		$('.calander').css('visibility', 'visible');
+		
+
+	});
 }
+
+$(document).ready(main);
