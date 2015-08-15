be7c1357d8080cb40648a5ce0222a428fdb745e0
KRapes <kerri.rapes@gmail.com>
2015-08-15 04:01:21 -0500

    Create home and calander navigation buttons

--- app.js
+++ app.js
@@ -5,8 +5,27 @@ var main = function(){
 		$('.banner').fadeOut(600, function(){
 			$('.calander').fadeIn(600);
 		});
+	});
+
+	$('.btn_cal').click(function() {
+		$('.pitch').fadeOut(600);
+		$('.banner').fadeOut(600, function(){
+			$('.calander').fadeIn(600);
+		});
 
 	});
+
+	$('.btn_home').click(function() {
+		$('.calander').fadeOut(600, function(){
+			$('.pitch').fadeIn(600);
+			$('.banner').fadeIn(600);
+		
+		});
+
+	});
+
+
+	
 }
 
 $(document).ready(main);
