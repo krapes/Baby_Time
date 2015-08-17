26be23aba097050251b173d3544b0b039fa3ffa5
KRapes <kerri.rapes@gmail.com>
2015-08-17 00:44:03 -0500

    Make nav_bar move from bottom of page to top based on media queries. Created icon and like to github. Added hover feature to calander months. Created viewAll function in js so that when viewport size moves from large style sheet to small/medium all features become present.

--- app.js
+++ app.js
@@ -1,31 +1,53 @@
+
 var main = function(){
+	console.log("Start main");
+	var mql = window.matchMedia("(min-width: 1200px)");
+	mql.addListener(handleWidthChange);
+	handleWidthChange(mql);
 	
-	$('.btn').click(function() {
-		$('.pitch').fadeOut(600);
-		$('.banner').fadeOut(600, function(){
+	$('.btn, #btn_cal').click(calIn);
+	$('#btn_home').click(homeIn);
+	$('m').hover(
+		function(){
+			$(this).css('background-color', '#B7D5F3');
+		},
+		function(){
+			$(this).css('background-color','#E3EAF1')
+		}
+	);
+
+}
+
+
+var calIn = function() {
+		$('.banner, .pitch').fadeOut(600, function(){
 			$('.calander').fadeIn(600);
 		});
-	});
+	}
 
-	$('.btn_cal').click(function() {
-		$('.pitch').fadeOut(600);
-		$('.banner').fadeOut(600, function(){
-			$('.calander').fadeIn(600);
-		});
-
-	});
-
-	$('.btn_home').click(function() {
+var homeIn = function() {
 		$('.calander').fadeOut(600, function(){
-			$('.pitch').fadeIn(600);
-			$('.banner').fadeIn(600);
-		
+			$('.pitch, .banner').fadeIn(600);
 		});
+	}
 
-	});
+var viewAll = function(){
+	$('.pitch, .banner').fadeIn(600);
+	$('.calander').fadeIn(600);
+}
 
+var handleWidthChange = function(mql)
+	{if (mql.matches) /* The viewport is currently > 1200px */
+		{	$('.art').before($('.nav_bar'));
+			homeIn();
+		}
+    
+    else /* The viewport is currently < 1200px */
+    {   
+    	$('.sixteen').after($('.nav_bar')); 
+    	viewAll();
+    }
 
-	
 }
 
 $(document).ready(main);
